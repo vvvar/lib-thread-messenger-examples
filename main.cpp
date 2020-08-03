58175ae32c622b0b2312be3d1cb74a37b9e2e036
Vlad Voinov <voinovvladv@gmail.com>
2020-08-03 02:47:03 +0200

    Register channel before thread creation;

--- main.cpp
+++ main.cpp
@@ -3,16 +3,14 @@
 using Messenger = messenger::Messenger;
 using MessageData = message::MessageData;
 
-class UserData : public MessageData
+class UserData: public MessageData
 {
 public:
   using UserName = std::string;
-  
   UserData(UserName user_name):
     MessageData(),
     _user_name(user_name)
   {}
-  
   UserName getUserName()
   {
     return _user_name;
@@ -21,35 +19,32 @@ private:
   UserName _user_name;
 };
 
-class RandomClass {};
-
-void Worker_1(std::shared_ptr<Messenger> messenger_ptr)
+void Worker_Thread(std::shared_ptr<Messenger> messenger_ptr)
 {
-  messenger_ptr->registerChannel("worker_1");
-  messenger_ptr->waitForMessageInChannel("worker_1");
-  auto user_data_ptr = messenger_ptr->receive<UserData>("worker_1");
-  std::cout << "Worker 1 got user data: " << user_data_ptr->getUserName() << ".\n";
+  messenger_ptr->waitForMessageInChannel("worker");
+  auto user_data_ptr = messenger_ptr->receive<UserData>("worker");
+  std::cout << "Worker got user data: " << user_data_ptr->getUserName() << ".\n";
 }
 
-void Worker_2(std::shared_ptr<Messenger> messenger_ptr)
+void UI_Thread(std::shared_ptr<Messenger> messenger_ptr)
 {
-  while(true) {
-    std::cout << "Enter your name: ";
-    std::string input;
-    std::getline (std::cin, input);
-    std::cin.clear();
-    auto user_data_ptr = std::make_shared<UserData>(input);
-    messenger_ptr->send<UserData>("worker_1", user_data_ptr);
-  }
+  std::cout << "Enter your name: ";
+  std::string input;
+  std::getline (std::cin, input);
+  std::cin.clear();
+  auto user_data_ptr = std::make_shared<UserData>(input);
+  messenger_ptr->send<UserData>("worker", user_data_ptr);
 }
 
 int main()
 {
   auto messenger_ptr = libthreadmessenger::MakeMessenger();
   
-  std::thread t1(Worker_1, messenger_ptr);
-  std::thread t2(Worker_2, messenger_ptr);
+  messenger_ptr->registerChannel("worker");
   
+  std::thread t1(Worker_Thread, messenger_ptr);
+  std::thread t2(UI_Thread, messenger_ptr);
+
   t2.detach();
   t1.join();
   
